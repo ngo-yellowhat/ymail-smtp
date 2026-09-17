26169912d70f30092c172fa30b8e471e8ab06466
Amnez3a <maksimalhimov549@gmail.com>
2026-09-17 23:03:40 +0300

    cmd: 'status' arg added

--- cmd/start.go
+++ cmd/start.go
@@ -1,40 +1,34 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"time"
 
+	"yellowsmtp/internal/inbound"
+
+	"github.com/emersion/go-smtp"
 	"github.com/spf13/cobra"
 )
 
-// startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
+		be := &inbound.Backend{}
+		s := smtp.NewServer(be)
+
+		s.Addr = ":2525"
+		s.Domain = "localhost"
+		s.WriteTimeout = 5 * time.Minute
+		s.ReadTimeout = 10 * time.Second
+		s.AllowInsecureAuth = true
+
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("[  SERVER  ] SMTP start on %s:%s", s.Domain, s.Addr)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

85ad133be16ab5e645d9aa9dad191beec573a8dc
Amnez3a <maksimalhimov549@gmail.com>
2026-09-17 22:29:30 +0300

    first commit to cobra-fork

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.26.7
 require (
 	github.com/emersion/go-sasl v0.0.0-20241020182733-b788ff22d5a6 // indirect
 	github.com/emersion/go-smtp v0.25.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.10.2 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
 )
--- main.go
+++ main.go
@@ -1,52 +1,11 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"flag"
-	"log"
-	"net"
-	"time"
-
-	"yellowsmtp/internal/inbound"
-	"yellowsmtp/internal/outbound"
-
-	"github.com/emersion/go-smtp"
-)
+import "yellowsmtp/cmd"
 
 func main() {
-	flagSend := flag.Bool("send", false, "automaticly send a test email after startup")
-	flag.Parse()
-
-	be := &inbound.Backend{}
-	s := smtp.NewServer(be)
-
-	s.Addr = ":2525"
-	s.Domain = "localhost"
-	s.ReadTimeout = 5 * time.Minute
-	s.WriteTimeout = 10 * time.Second
-	s.AllowInsecureAuth = true
-
-	if *flagSend {
-		go func() {
-			log.Println("[  SERVER  ] SMTP start on :2525")
-			if err := s.ListenAndServe(); err != nil {
-				log.Fatalf("[  ERROR  ] Server: %v", err)
-			}
-		}()
-		for {
-			conn, err := net.Dial("tcp", "localhost:2525")
-			if err == nil {
-				conn.Close()
-				break
-			}
-			time.Sleep(60 * time.Millisecond)
-		}
-		log.Println("[  TEST  ] Email sending test..")
-		outbound.Send()
-		time.Sleep(500 * time.Millisecond)
-		return
-	}
-	log.Println("[  SERVER  ] SMTP start on :2525")
-	if err := s.ListenAndServe(); err != nil {
-		log.Fatalf("[  ERROR  ] Server: %v", err)
-	}
+	cmd.Execute()
 }

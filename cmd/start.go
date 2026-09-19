551ae8d0ac4e51dbfa70718f7167711cbda8ba1f
Amnez3a <maksimalhimov549@gmail.com>
2026-09-19 17:42:26 +0300

    cmd: add flags '--port' & '--domain'

--- cmd/start.go
+++ cmd/start.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -10,6 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var domain string
+var port int
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "A brief description of your command",
@@ -17,18 +20,21 @@ var startCmd = &cobra.Command{
 		be := &inbound.Backend{}
 		s := smtp.NewServer(be)
 
-		s.Addr = ":2525"
-		s.Domain = "localhost"
+		s.Addr = fmt.Sprintf(":%d", port)
+		s.Domain = fmt.Sprintf("%s", domain)
 		s.WriteTimeout = 5 * time.Minute
 		s.ReadTimeout = 10 * time.Second
 		s.AllowInsecureAuth = true
 
+		log.Printf("[  SERVER  ] SMTP start on %s%s", s.Domain, s.Addr)
 		if err := s.ListenAndServe(); err != nil {
-			log.Printf("[  SERVER  ] SMTP start on %s:%s", s.Domain, s.Addr)
+			log.Printf("[  ERROR  ] SMTP server: %v", err)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
+	startCmd.Flags().IntVarP(&port, "port", "p", 25, "Set port for SMTP server")
+	startCmd.Flags().StringVarP(&domain, "domain", "d", "localhost", "Set domain for SMTP server")
 }
